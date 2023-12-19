55e4ce0f6fd244fb083bb3082ef32172322db76d
Akuak-Mayen <mayenakuak84@gmail.com>
2023-12-19 08:04:47 -0800

    updating Review & her r-ships

--- models/user.py
+++ models/user.py
@@ -5,14 +5,22 @@
 """
 
 from models.base_model import BaseModel
-
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 class User(BaseModel):
     """User representation"""
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+  __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+
+    places = relationship('Place', cascade='all, delete, delete-orphan',
+                          backref='user')
+
+    reviews = relationship('Review', cascade='all, delete, delete-orphan',
+                           backref='user')
 
     def __init__(self, *args, **kwargs):
         """User initialization"""
